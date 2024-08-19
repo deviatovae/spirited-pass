0a13965cba8d9345e5890d0031772d64f0636610
Alena Deviatova <aedeviatova@yandex.ru>
2024-08-19 15:56:01 -0400

    fix docker, add swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Spirit Pass API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('openapi', app, document);
+
+  writeFile('/common/apiSchema.json', JSON.stringify(document));
+
   await app.listen(3000);
 }
 bootstrap();
